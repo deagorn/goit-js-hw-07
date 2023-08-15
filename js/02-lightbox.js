3a48d0d33bb42ba3366f0b440e8447eab0ade793
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-15 18:04:16 +0300

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// Creating a gallery 
+const markup = galleryItems
+    .map((items) => `<li class="gallery__item">
+   <a class="gallery__link" href="${items.original}">
+      <img class="gallery__image" src="${items.preview}" alt="${items.description}" />
+   </a>
+</li>`)
+    .join("");
+
+// Adding a gallery to HTML
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = markup;
+
+
+ // creating a modal window
+    var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt',
+     captionDelay: 250  });
+
+
